322d88a70d0205b2aa84ecf277abac8bf6542923
khaisheen <41457502+khaisheen@users.noreply.github.com>
2020-06-05 01:09:18 +0800

    added click detection thru sockets

--- mouseio.py
+++ mouseio.py
@@ -1,15 +1,22 @@
 from pynput.mouse import Listener, Button, Controller
 import socketio, eventlet, numpy as np
 
+# Server stuff
 HOST = 'localhost'
 PORT = 3000
 ADDR = (HOST,PORT)
+
 mouse = Controller()
+clicked = False
+
+# =============================================================================
+# Listener for clicks
+# =============================================================================
 
 def on_click(x, y, button, pressed):
-    if button == Button.right:
-        return False
-    print("CLICK!!!, pressed=",pressed)
+    global clicked
+    print("CLICK!!!")
+    clicked = pressed
 
 # =============================================================================
 # SocketIO server
@@ -32,25 +39,30 @@ def disconnect(sid):
     CONNECTED = False
     print('disconnect ', sid)
     
+# Emit stuff here
 @sio.on('my data', namespace='/')
 def mydata_handler(sid, msg):
+    global clicked
     print('Message received:', msg)
     while CONNECTED:
         event = 'my data'
-        data = [mouse.position[0], mouse.position[1]]
+        data = {'x':mouse.position[0],
+                'y':mouse.position[1],
+                'pressed':clicked}
         sio.emit(event, data)
         print('Emitted:', event, data, '\n')
         sio.sleep(1/30)
 
-
+# To turn off the server u gotta close the console, ctrl+C dont work
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(ADDR),app)    
     try:
         listener = Listener(on_click=on_click)
-        listener.start()
-        while listener.running:
+        listener.start()    
+        eventlet.wsgi.server(eventlet.listen(ADDR),app)    
+#        while listener.running:
+#            print(f"Listener:{clicked}")
 #            print(f"x={mouse.position[0]}, y={mouse.position[1]}")
-            sio.sleep(1/30)
+#            sio.sleep(1/30)
     except KeyboardInterrupt:
         listener.stop()
             
