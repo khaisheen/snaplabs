c0a4f761ba2700b2c8b9656257c80e9363285b9b
PTMZ <paultmz88@gmail.com>
2020-07-23 02:36:41 +0800

    Pathfinding multilevel done

--- Source/circlesMalone/CustomPathfindingLibrary.h
+++ Source/circlesMalone/CustomPathfindingLibrary.h
@@ -14,6 +14,9 @@ class CIRCLESMALONE_API UCustomPathfindingLibrary : public UBlueprintFunctionLib
 {
 	GENERATED_BODY()
 	UFUNCTION(BlueprintCallable, Category = "MyCustomLibrary")
-		static TArray<FVector2D> getPath(int gx, int gy);
+		static TArray<FVector> getPath(int sx, int sy, int gx, int gy, int z);
+
+	UFUNCTION(BlueprintCallable, Category = "MyCustomLibrary")
+		static TArray<FVector> getFullPath(int sx, int sy, int sz, int gx, int gy, int gz);
 		
 };
