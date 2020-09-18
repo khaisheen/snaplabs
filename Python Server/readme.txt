d2188cf4e1935558bc4221f5df26b25e20dd2e8f
PTMZ <paultmz88@gmail.com>
2020-09-19 00:16:29 +0800

    Cleaned Project without Victory Plugin, Restart from scratch

--- Python Server/readme.txt
+++
@@ -1,4 +0,0 @@
-Instructions for running python backend program:
-1. $ cd <project root>/Python Server
-2. $ python main.py
-3. Close the terminal to terminate the progam
---
+++ Source/snaplabsMap/CustomPathfindingLibrary.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CustomPathfindingLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAPLABSMAP_API UCustomPathfindingLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	UFUNCTION(BlueprintCallable, Category = "MyCustomLibrary")
+		static TArray<FVector> getPath(int sx, int sy, int gx, int gy, int z);
+
+	UFUNCTION(BlueprintCallable, Category = "MyCustomLibrary")
+		static TArray<FVector> getFullPath(int sx, int sy, int sz, int gx, int gy, int gz);
+		
+};
