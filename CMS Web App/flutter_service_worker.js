345fdd16f18ef47423729046f0d8e9d8b7ddfb36
khaisheen <41457502+khaisheen@users.noreply.github.com>
2020-09-20 15:21:50 +0800

    updated flutter web app

--- CMS Web App/flutter_service_worker.js
+++ CMS Web App/flutter_service_worker.js
@@ -8,14 +8,14 @@ const RESOURCES = {
 "assets/assets/events": "121ea6e3d0046ef9122782ff458903bd",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "57120446a1e64c92034483d675fc8a80",
+"assets/NOTICES": "2a5761f3653c66d5464c918169100ca0",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "13b9b831f43674adfb5c07c1e1b1487b",
 "/": "13b9b831f43674adfb5c07c1e1b1487b",
-"main.dart.js": "15e34dd7343e58b8725617d29213f1f6",
+"main.dart.js": "9c6d182af6fbeb357cd645002172eecd",
 "manifest.json": "8b7dc9720d9ebb4adbaccb75016d37ed"
 };
 
