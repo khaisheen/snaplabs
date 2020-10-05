611f46112a4b7e844e292dfe5f934e9e1e41227a
khaisheen <41457502+khaisheen@users.noreply.github.com>
2020-10-05 18:41:54 +0800

    update pose.sh to cd before running

--- bash/startup/pose.sh
+++ bash/startup/pose.sh
@@ -2,7 +2,9 @@
 
 echo starting up pose estimator...
 
-python3 ../../pose_est/demo13.py --model human-pose-estimation-3d.pth --video 0
+cd "../../pose_est"
+
+python3 demo13.py --model human-pose-estimation-3d.pth --video 0
 
 echo DONE! Pose estimator up and running.
 
